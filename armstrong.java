dafb615fb49798baeb2ad08cf844f7639adf8a5b
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-09-20 19:55:01 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,31 @@
+/*
+import java.util.Scanner;
+
+public class armstrong {
+   
+    public static void main(String args[]){
+        int i,n,sum=0;
+        int r,q;
+        System.out.println("enter the number");         
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        q=n;
+        while(n>0)
+        {
+            r=n%10;
+            sum=(r*r*r)+sum;
+            n=n/10;
+
+        }
+        if(q==sum)
+        {
+            System.out.println("armstrong number");       
+        }
+        else
+        {
+             System.out.println("not armstrong number");
+        }
+
+}
+}
+*/
---
+++ palindrome.java
@@ -0,0 +1,31 @@
+/*
+import java.util.Scanner;
+
+public class palindrome {
+   
+    public static void main(String args[]){
+        int i,n,rev=0;
+        int r,q;
+        System.out.println("enter the number");         
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        q=n;
+        while(n>0)
+        {
+            r=n%10;
+            rev=(rev*10)+r;
+            n=n/10;
+
+        }
+        if(q==rev)
+        {
+            System.out.println("palindrome number");       
+        }
+        else
+        {
+             System.out.println("not palindrome number");
+        }
+
+}
+}
+*/
