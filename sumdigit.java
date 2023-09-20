5746f2430a107c785d1e75ff8bf568c6de49f175
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-09-20 19:14:00 +0530

    Add files via upload

---
+++ sumdigit.java
@@ -0,0 +1,20 @@
+/*
+import java.util.Scanner;
+
+public class sumdigit {
+    public static void main(String args[]){
+        int i,r,n,sum=0;
+        System.out.println("enter the number");         
+        Scanner sc = new Scanner(System.in);            //enter 3473=17 is sum
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+
+        }
+        System.out.println("the sum of digit is : "+sum);     
+
+}
+}*/
