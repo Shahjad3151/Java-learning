9f1e6203869408f8b2f1f47a2777f010f13b08b5
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-09-24 00:19:03 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,45 @@
+/* 
+import java.util.Scanner;
+
+public class five {
+    public static void main (String args[]) {
+        int num;                                  // grade problem
+        Scanner obj = new Scanner(System.in);     
+        System.out.print("enter the first number "); 
+        num=obj.nextInt();
+       /*
+        if(num>80 && num<=100)
+        {
+            System.out.println("grade A");
+        }
+        else if(num>60 && num<=80)
+        {
+            System.out.println("grade B");
+        }
+        else
+        {
+            System.out.println("grade c");
+        }
+        */
+     /*    switch(num){
+            case 0: System.out.println("sunday");   
+                   break;                           //break ni lagaiyenge to sb print hoga
+            case 1: System.out.println("monday");
+                   break;
+            case 2: System.out.println("tuesday");
+                   break;
+            case 3: System.out.println("wednesday");
+                   break;
+            case 4: System.out.println("thrusday");
+                   break;
+            case 5: System.out.println("friday");
+                   break;
+            case 6: System.out.println("saturday");
+                   break;
+            default:System.out.println("invalid");
+                   break;
+        }
+}
+}
+*/
+
