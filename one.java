f56101b97f3c727ba7a2ff9b7a002db546801181
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-09-25 00:56:55 +0530

    Add files via upload

--- one.java
+++ one.java
@@ -1,22 +1,45 @@
-import java.util.Scanner;
-
+/* 
 public class one {
-     public static void main (String args[]) {
-        int a,b;                                  
-        Scanner obj = new Scanner(System.in);     
-        System.out.print("enter the first number "); 
-        a=obj.nextInt();
-        System.out.print("enter the first number "); 
-        b=obj.nextInt();
-        //input a=8 b= 11
-        System.out.println(++a);    //a=9  
-        System.out.println(a++);    //a=9 assign
-        System.out.println(b--);    //b=11 assign
-        System.out.println(--b);    //b=9 
-        System.out.println(++a + b--);  //a=11 b= 9 aassign 20
-        System.out.println(++b - a++);  //b=9 a=11 assign -2
-        System.out.println(a++ - ++b);  //a=12 assign  b=10 2
-        System.out.println(--a + --b);  //a=12 b=9 21
-        System.out.println(--b - --a);  //b=8 a=11 -3
-}
-}
+    public static void main(String args[]){
+    // System.out.println("Hi");               # print Hi
+    // System.out.println("Hi Hello");                  # print Hi Hello    
+    /*
+       System.out.println("Hi");                    #print     Hi 
+       System.out.println("Hello");                            Hello   
+    */
+    // System.out.println("*");                 #print  *
+    // System.out.println("**");                             #print  **
+    /*
+       System.out.println("*");                 # print         *
+       System.out.println("**");                                **
+       System.out.println("***");                               ***
+       System.out.println("****");                              ****
+       System.out.println("*****");                             *****
+    */
+    /* 
+    System.out.println("*****");                # print       *****
+    System.out.println(" ****");                               **** 
+    System.out.println("  ***");                                ***
+    System.out.println("   **");                                 **
+    System.out.println("    *");                                  *
+    */
+    /* 
+    System.out.println("*");                // # print      *
+    System.out.println("**");                               **
+    System.out.println("***");                              ***
+    System.out.println("****");                             ****
+    System.out.println("*****");                            *****
+    System.out.println(" ****");                             **** 
+    System.out.println("  ***");                              ***
+    System.out.println("   **");                               **
+    System.out.println("    *");                                *
+    */
+    /* 
+    System.out.println("    *    ");            // # print    *
+    System.out.println("   * *   ");                         * *
+    System.out.println("  * * *  ");                        * * *
+    System.out.println(" * * * * ");                       * * * *
+    */
+    
+//}
+//}
