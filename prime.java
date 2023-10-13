4e512da9f4551e93f4a2f3f0efe5d2b02620a1a1
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-10-13 23:47:38 +0530

    Add files via upload

--- prime.java
+++ prime.java
@@ -1,26 +1,33 @@
 /*
 import java.util.Scanner;
-
-public class prime {
+public class prime{
     public static void main(String args[]){
-        int i,n;
-        System.out.println("enter the number"); //prime number jo khudse & 1 se divisible
-        Scanner sc = new Scanner(System.in);
-      
+        prime obj=new prime();              //class object
+        System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);      //user input
+        int n;
         n=sc.nextInt();
-        for(i=2;i<n;i++)
-        {   
-            if (n%i==0)
-            {
-            System.out.println(" not prime number ");
-            break;
-            }
-            else
-            {
-            System.out.println("  prime number ");
-            break;
-            }
+        obj.prim(n); //calling
+    }
+    void prim(int n){
+    int i;
+    for(i=2;i<n;i++)
+    {   
+        if (n%i==0)
+        {
+            
+        System.out.println(" not prime number ");
+        
+        break;
+        
         }
-        sc.close();
+        else
+        {
+        System.out.println("  prime number ");
+        break;
+        
+        }
+    }
+   } 
 }
-}*/
+*/
