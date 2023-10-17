aca638219e53edfae1a597524e01b1e4297b9165
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2023-10-17 23:36:00 +0530

    Add files via upload

--- one.java
+++ one.java
@@ -1,45 +1,67 @@
 /* 
+import java.util.Scanner;
+
 public class one {
-    public static void main(String args[]){
-    // System.out.println("Hi");               # print Hi
-    // System.out.println("Hi Hello");                  # print Hi Hello    
-    /*
-       System.out.println("Hi");                    #print     Hi 
-       System.out.println("Hello");                            Hello   
-    */
-    // System.out.println("*");                 #print  *
-    // System.out.println("**");                             #print  **
-    /*
-       System.out.println("*");                 # print         *
-       System.out.println("**");                                **
-       System.out.println("***");                               ***
-       System.out.println("****");                              ****
-       System.out.println("*****");                             *****
-    */
-    /* 
-    System.out.println("*****");                # print       *****
-    System.out.println(" ****");                               **** 
-    System.out.println("  ***");                                ***
-    System.out.println("   **");                                 **
-    System.out.println("    *");                                  *
-    */
-    /* 
-    System.out.println("*");                // # print      *
-    System.out.println("**");                               **
-    System.out.println("***");                              ***
-    System.out.println("****");                             ****
-    System.out.println("*****");                            *****
-    System.out.println(" ****");                             **** 
-    System.out.println("  ***");                              ***
-    System.out.println("   **");                               **
-    System.out.println("    *");                                *
-    */
-    /* 
-    System.out.println("    *    ");            // # print    *
-    System.out.println("   * *   ");                         * *
-    System.out.println("  * * *  ");                        * * *
-    System.out.println(" * * * * ");                       * * * *
-    */
+     public static void main (String args[]) {
+         int a,b,c;                                  
+        Scanner obj = new Scanner(System.in);     
+        System.out.print("enter the first number "); 
+        a=obj.nextInt();
+        System.out.print("enter the second number "); 
+        b=obj.nextInt();
+/* 
+                                          //input a=8 b= 11 increment decrement operator
+
+        System.out.println(++a);    //a=9  
+        System.out.println(a++);    //a=9 assign
+        System.out.println(b--);    //b=11 assign
+        System.out.println(--b);    //b=9 
+        System.out.println(++a + b--);  //a=11 b= 9 aassign 20
+        System.out.println(++b - a++);  //b=9 a=11 assign -2
+        System.out.println(a++ - ++b);  //a=12 assign  b=10 2
+        System.out.println(--a + --b);  //a=12 b=9 21
+        System.out.println(--b - --a);  //b=8 a=11 -3
+       
+        System.out.println(10/2+5*3-4/2+3*2);  //24 priority (),^,*,/,+,-
+      
+      // arithmatic operator
+       System.out.println(a+b); 
+       System.out.println(a-b); 
+       System.out.println(a*b); 
+       System.out.println(a/b); 
+       System.out.println(a%b); 
+ 
+     // relational operator
+        System.out.println(a>b); 
+        System.out.println(a>=b); 
+        System.out.println(a<b); 
+        System.out.println(a<=b); 
+        System.out.println(a==b); 
+        System.out.println(a!=b); 
+
+    // logical operator
+       System.out.print("enter the second number "); 
+       c=obj.nextInt();
+       System.out.println((a>b) && (a>c));
+       System.out.println((a>b) || (a>c));
+       System.out.println((a>b) && (c>a));
+       System.out.println(!(a>b) || (c>a));
+  
+    // relational operator  
+       a=9;
+       a+=10;
+       System.out.println(a);
     
+    // ternary operator  
+       int max;
+       max=(a>b)?a:b;
+       System.out.println(max);
+*/ 
+/* 
+    // bitwise operator
+       System.out.println(a & b);
+       System.out.println(a | b);
+       System.out.println(a ^ b);
+ */     
 //}
 //}
